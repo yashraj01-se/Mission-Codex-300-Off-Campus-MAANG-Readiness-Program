1b2c4f8aa72b71cc9d94e76dfdb24ce0b529b5b0
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2025-12-12 13:02:27 +0530

    12-12-25

--- Two Pointer/Pair Sum/Better_Solution.cpp
+++ Two Pointer/Pair Sum/Better_Solution.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-vector<int>Two_sum_Better_Solution(vector<int>&arr,int target){
+vector<int> Two_sum_Better_Solution(vector<int> &arr, int target)
+{
 
-    unordered_map<int,int>mp;
-    int n=arr.size();
+    unordered_map<int, int> mp;
+    int n = arr.size();
 
-    for(int i=0;i<n;i++){
-        int left=target-arr[i];
-        if(mp.find(left)!=mp.end()){
-            return {mp[left],i};
+    for (int i = 0; i < n; i++)
+    {
+        int left = target - arr[i];
+        if (mp.find(left) != mp.end())
+        {
+            return {mp[left], i};
         }
         // store the value and index for future reference...
-        mp[arr[i]]=i;
+        mp[arr[i]] = i;
     }
-    return {-1,-1};
+    return {-1, -1};
 }
 
 int main()
